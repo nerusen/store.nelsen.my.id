177ede1bf4eda2435894266aabdd95e38b9f65fe
nerusen <162538024+nerusen@users.noreply.github.com>
2025-12-30 02:44:43 +0000

    Add files via upload

--- modules/chat/components/ChatItemSkeleton.tsx
+++ modules/chat/components/ChatItemSkeleton.tsx
@@ -1,5 +1,4 @@
 import SkeletonLoader from "@/common/components/elements/SkeletonLoader";
-import Skeleton from "react-loading-skeleton";
 
 const ChatItemSkeleton = () => {
   return (
@@ -7,37 +6,37 @@ const ChatItemSkeleton = () => {
       <div className="space-y-5 py-4">
         {/* Skeleton 1 */}
         <div className="flex items-center gap-3 px-4 lg:px-8">
-          <Skeleton className="h-[40px] w-[40px] rounded-full" />
+          <div className="h-[40px] w-[40px] bg-gray-300 dark:bg-gray-700 rounded-full"></div>
           <div className="space-y-1 flex-1">
             <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1">
-              <Skeleton containerClassName="w-20" />
+              <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-20"></div>
             </div>
             <div className="group flex flex-col w-fit relative">
-              <Skeleton containerClassName="w-64" className="h-12 rounded-xl" />
+              <div className="h-12 bg-gray-300 dark:bg-gray-700 rounded-xl w-64"></div>
             </div>
           </div>
         </div>
         {/* Skeleton 2 */}
         <div className="flex items-center gap-3 px-4 lg:px-8 flex-row-reverse">
-          <Skeleton className="h-[40px] w-[40px] rounded-full" />
+          <div className="h-[40px] w-[40px] bg-gray-300 dark:bg-gray-700 rounded-full"></div>
           <div className="space-y-1 flex-1 flex flex-col items-end">
             <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1 md:flex-row-reverse">
-              <Skeleton containerClassName="w-20" />
+              <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-20"></div>
             </div>
             <div className="group flex flex-col w-fit relative items-end">
-              <Skeleton containerClassName="w-48" className="h-10 rounded-xl" />
+              <div className="h-10 bg-gray-300 dark:bg-gray-700 rounded-xl w-48"></div>
             </div>
           </div>
         </div>
         {/* Skeleton 3 */}
         <div className="flex items-center gap-3 px-4 lg:px-8">
-          <Skeleton className="h-[40px] w-[40px] rounded-full" />
+          <div className="h-[40px] w-[40px] bg-gray-300 dark:bg-gray-700 rounded-full"></div>
           <div className="space-y-1 flex-1">
             <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1">
-              <Skeleton containerClassName="w-16" />
+              <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-16"></div>
             </div>
             <div className="group flex flex-col w-fit relative">
-              <Skeleton containerClassName="w-56" className="h-14 rounded-xl" />
+              <div className="h-14 bg-gray-300 dark:bg-gray-700 rounded-xl w-56"></div>
             </div>
           </div>
         </div>
